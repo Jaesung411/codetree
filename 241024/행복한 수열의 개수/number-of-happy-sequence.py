f6749e329a5372acce879a33e2fda51f584ccd87
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-24 22:46:50 +0900

    Title: 행복한 수열의 개수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241024/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+def check(l):
+    pn = l[0]
+    cnt = 1
+    for i in range(1,n):
+        if pn == l[i]:
+            cnt += 1
+        else:
+            pn = l[i]
+            cnt= 1
+
+        if cnt == m:
+            return True
+    return False
+ans = 0
+for row in graph:
+    if check(row):
+        ans+=1
+for col in [[row[i] for row in graph] for i in range(n)]:
+    if check(col):
+        ans+=1
+print(ans)

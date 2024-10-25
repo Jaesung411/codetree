756d3cd5e8dd302e9a2d751e236f14d57341bc6d
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-25 14:41:06 +0900

    Title: 기울어진 직사각형, Time: 226ms, Memory: 26MB, Status: Passed - CodeTree

--- 241025/기울어진 직사각형/slanted-rectangle.py
+++ 241025/기울어진 직사각형/slanted-rectangle.py
@@ -1,39 +1,28 @@
-def possible_square(x,y):
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if not (0<= nx < N and 0 <= ny < N):
-            return False
-    return True
-
-#(x,y)에서 최댓값 출력
-def solve(x,y):
+def solve(x,y,up,down):
+    dx = [1,-1,-1,1]
+    dy = [-1,-1,1,1]
+    nums_dir = [up,down,up,down]
     s = 0
-    ss = [0,0]
-    #(x,y)에서 가능한지
-    if possible_square(x,y):
-        for i in range(4):
-            ss[0] += graph[y+dy[i]][x+dx[i]]
-            ss[1] += graph[y+dy[i]][x+dx[i]]
-        
-        for sn, dxn, dyn in [(0,1,-1),(1,-1,-1)]:
-            nx, ny = x + dxn, y + dyn
-            #연장 가능하다면
-            while possible_square(nx,ny):
-                ss[sn] += graph[ny-2][nx]
-                ss[sn] += graph[ny+dxn][nx+dyn]
-                nx, ny = nx + dxn, ny + dyn
-                
-        s = max(ss)
-
+    nx = x
+    ny = y
+    for i, n in enumerate(nums_dir):
+        for j in range(n):
+            nx += dx[i]
+            ny += dy[i]
+            if 0<=nx<N and 0<=ny<N:
+                s+=graph[ny][nx]
+            else:
+                return 0
+   
     return s
 
 N = int(input())
 graph = [list(map(int,input().split())) for _ in range(N)]
 ans = 0
-dx = [1,0,-1,0]
-dy = [-1,-2,-1,0]
 
 for col in range(N):
     for row in range(N):
-        ans = max(ans,solve(row,col))
+        for up in range(1,N):
+            for down in range(1,N):
+                ans = max(ans,solve(row,col,up,down))
 print(ans)

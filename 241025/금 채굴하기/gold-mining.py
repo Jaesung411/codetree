d4db8e2e4418bbc59d8eb4f4a844d75d043b1ac2
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-25 10:59:06 +0900

    Title: 금 채굴하기, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241025/금 채굴하기/gold-mining.py
@@ -0,0 +1,40 @@
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+#손해 보지 않는 채굴의 금의 개수
+def solve(x,y):
+    ret = 0
+    #채굴의 크기 k
+    for k in range(1,n-1):
+        #범위 안에 채굴되는 위치들
+        shapes =[]
+        for i in range(-k,k+1):
+            for j in range(-k,k+1):
+                if abs(i) + abs(j) <= k:
+                    if in_range(x+j,y+i):
+                        shapes.append((x+j,y+i))
+
+        #위치 안에 골드 수
+        n_gold = 0
+        for px,py in shapes:
+            if graph[py][px] == 1:
+                n_gold += 1
+        # 비용 계산
+        cost = m * n_gold - (k*k+(k+1)*(k+1))
+        if cost<0:
+            continue
+        if n_gold == 4: 
+            print(shapes,len(shapes))
+            print(x,y,k,cost)
+        ret = max(ret,n_gold)
+    return ret
+        
+
+n,m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+mx_gold = 0
+
+for i in range(n):
+    for j in range(n):
+        mx_gold = max(mx_gold,solve(j,i))
+print(mx_gold)

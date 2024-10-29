43a9cb20480c13d7a14c41b20eb7fe97f258be4d
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-29 22:35:53 +0900

    Title: 1차원 폭발 게임, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241029/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,34 @@
+def get_endpoint(start,number):
+    for end in range(start+1,len(numbers)):
+        if number != numbers[end]:
+            return end-1
+    return len(numbers) - 1
+
+N, M = map(int,input().split())
+numbers = [int(input()) for _ in range(N)]
+
+while True:
+    did_explore = False
+    for start, number in enumerate(numbers):
+        #벌써 터진 포탄인 경우
+        if number == 0:
+            continue
+        
+        #같은 number를 가지는 마지막 지점
+        end = get_endpoint(start, number)
+        #M번이상 일 경우 0 
+        if end - start +1 >= M:
+            did_explore = True
+            for i in range(start,end+1):
+                numbers[i] = 0
+        
+        temp = []
+        for i in range(len(numbers)):
+            if numbers[i] != 0:
+                temp.append(numbers[i])
+
+        numbers = temp.copy() 
+    
+    if not did_explore:
+        break
+print(len(numbers))

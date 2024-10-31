d5841b411c57afa05a61aa79aab5d58e5b98caae
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-31 11:12:17 +0900

    Title: 최소 와이파이 수, Time: 5083ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 241031/최소 와이파이 수/minimum-number-of-wifi.py
+++ 241031/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,10 +1,15 @@
 N,M = map(int,input().split())
 people = list(map(int,input().split()))
-poss_internet = [False] * N
+
 ans = 0
-for i in range(N):
-    if people[i] and not poss_internet[i]:
-        for j in range(i,i+2*M+1):
-            poss_internet[j] = True
+i = 0
+while i != N:
+    if people[i]:
+        i+= 2*M+1
         ans+=1
+# for i in range(N):
+#     if people[i] and not poss_internet[i]:
+#         for j in range(i,i+2*M+1):
+#             poss_internet[j] = True
+        # ans+=1
 print(ans)

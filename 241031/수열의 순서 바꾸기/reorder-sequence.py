ce4a2fd43324da8c0381fdf0ccde6d168c02577d
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-31 12:38:26 +0900

    Title: 수열의 순서 바꾸기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241031/수열의 순서 바꾸기/reorder-sequence.py
+++ 241031/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,16 +1,14 @@
 N = int(input())
 arr = list(map(int,input().split()))
 
-mx = 0
-mn = float('inf')
-ans = 0
+arr_sorted = list(sorted(arr))
+
+ans = -1
 for i in range(N):
-    ans += 1
-    if arr[i] < mn:
-        mn = arr[i]
-    elif arr[i] > mx:
-        mx = arr[i]
-    else:
-        ans -= 1
-        break
-print(ans)
+    if arr[i] != arr_sorted[i]:
+        ans = i
+
+if ans == -1:
+    print(0)
+else:
+    print(ans)

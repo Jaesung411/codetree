352a93c31f7590032ce6db7f1bdd09539b6a9164
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-10-31 12:12:33 +0900

    Title: 홀수 짝수의 묶음, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/홀수 짝수의 묶음/odd-even-bundle.py
+++ 241031/홀수 짝수의 묶음/odd-even-bundle.py
@@ -1,18 +1,27 @@
 N = int(input())
 arr = list(map(int,input().split()))
 ans = 0
-i = 0
-flag = 0
-while i < N-1:
-    # print("start",i)
-    s = arr[i] + arr[i+1]
-    i+=2
-    if i >= N: 
-        ans+=1
+even = 0
+odd = 0
+for i in range(N):
+    if arr[i] % 2 == 1:
+        odd += 1
+    else:
+        even += 1
+
+while not ( odd == 0 and even == 0):
+    if even != 0:
+        even -= 1
+    elif odd >= 2:
+        odd -= 2
+    else:
+        ans -= 1
         break
-    while flag%2 == s%2:
-        s += arr[i]
-        i+=1
-    flag+=1 
-    ans+=1
+    ans += 1
+
+    if odd != 0:
+        odd -= 1
+    else:
+        break
+    ans += 1
 print(ans)

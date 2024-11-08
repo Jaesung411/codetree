13817fa3ffa89feb769f7038ce918cfdd2cdad95
jaesung411 <97060253+Jaesung411@users.noreply.github.com>
2024-11-08 15:37:46 +0900

    Title: 단 한 번의 2048 시도, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241108/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,49 @@
+def move_left(row):
+    temp = []
+    pnum = row[0] # 현재 저장된 숫자
+    for i in range(1,4):
+        if row[i] == 0 : 
+            continue
+        if pnum == row[i]:
+            temp.append(pnum*2)
+            pnum = 0
+        elif pnum != 0:
+            temp.append(pnum)
+            pnum = row[i]
+        else:
+            pnum = row[i]
+
+    if pnum != 0:
+        temp.append(pnum)
+
+    while len(temp) != 4:
+        temp.append(0)
+
+    return temp
+
+grid = [list(map(int,input().split())) for _ in range(4)]
+direction = input()
+ret = []
+
+if direction == 'L':
+    for i in range(4):
+        ret.append(move_left(grid[i]))
+elif direction == 'R':
+    for i in range(4):
+        ret.append(list(reversed(move_left(list(reversed(grid[i]))))))
+ret=[[],[],[],[]]
+if direction == 'U':
+    for i in range(4):
+        rotate_l = [grid[j][i] for j in range(4)]
+        temp = move_left(rotate_l)
+        for k in range(4):
+            ret[k].append(temp[k])
+elif direction == 'D':
+    for i in range(4):
+        rotate_l = [grid[j][i] for j in range(4)]
+        temp = move_left(list(reversed(move_left(list(reversed(rotate_l))))))
+        for k in range(4):
+            ret[k].append(temp[k])
+            
+for i in range(4):
+    print(" ".join(map(str,ret[i])))
